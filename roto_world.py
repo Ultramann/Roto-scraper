0d5d95101f52cbc2c5fb03219ac9f5ecde71ae00
Ultramann <cary.goltermann@gmail.com>
2015-08-24 10:16:54 -0600

    Something was broken, I fixed it!

--- roto_world.py
+++ roto_world.py
@@ -10,13 +10,14 @@ class RWScraper(object):
     def __init__(self):
         self.url = 'http://www.rotoworld.com/playernews/nfl/football-player-news'
         self._team_news = dd(lambda : dd(list))
-        self._same_day = True
-        self._date = (0, date.today().day, 0)
+        self.same_day = True
+        self.date = (0, date.today().day, 0)
 
     def _get_player_info(self, player_box):
+        #import pdb; pdb.set_trace()
         date_str = player_box.select('.date')[0].contents[0]
         date = int(re.findall(r'^.* (\d+?)[ ,].*$', date_str)[0])
-        if date != self._date[1]:
+        if date != self.date[1]:
             return False
         player_dict = {}
         player_info = player_box.select('.player a')
@@ -37,20 +38,20 @@ class RWScraper(object):
     def _get_player_news(self):
         driver = webdriver.PhantomJS()
         driver.get(self.url)
-        if self._date[0]:
+        if self.date[0]:
             date_picker = driver.find_element_by_id('tbDatepicker')
             date_picker.click()
-            date_picker.send_keys('{}/{}/{}'.format(self._date[0], self._date[1], self._date[2]))
+            date_picker.send_keys('{}/{}/{}'.format(self.date[0], self.date[1], self.date[2]))
             driver.find_element_by_id('cp1_ctl01_btnFilterResults').click()
-        while self._same_day:
+        while self.same_day:
             soup = BeautifulSoup(driver.page_source, 'html.parser')
             player_boxes = soup.select('.pb')
-            self._same_day = self._boxes_to_news(player_boxes)
+            self.same_day = self._boxes_to_news(player_boxes)
             driver.find_element_by_id('cp1_ctl01_btnNavigate1').click()
 
     def scrape(self, date='today'):
         if date != 'today':
-            self._date = date
+            self.date = date
         self._get_player_news()
         os.remove('ghostdriver.log')
 
@@ -61,10 +62,12 @@ class RWScraper(object):
                'Saints':    {'fg': 136, 'bg': 0},
                'Chargers':  {'fg': 226, 'bg': 21},
                'Lions':     {'fg': 27, 'bg': 244}, 
+               'Cowboys':   {'fg': 244, 'bg': 27}, 
                'Browns':    {'fg': 202, 'bg': 52},
                'Eagles':    {'fg': 231, 'bg': 29},
                'Steelers':  {'fg': 11, 'bg': 0}, 
                'Giants':    {'fg': 9, 'bg': 19},
+               'Patriots':  {'fg': 9, 'bg': 19},
                'Buccaneers':{'fg': 0, 'bg': 9}, 
                'Cardinals': {'fg': 0, 'bg': 1},
                'Chiefs':    {'fg': 196, 'bg': 0},
@@ -80,7 +83,8 @@ class RWScraper(object):
                'Texans':    {'fg': 18, 'bg': 196},
                '49ers':     {'fg': 15, 'bg': 124},
                'Seahawks':  {'fg': 10, 'bg': 21},
-               'Panthers':  {'fg': 0, 'bg': 33}}
+               'Panthers':  {'fg': 0, 'bg': 33},
+               'Bears':     {'fg': 208, 'bg': 17}}
         for team in self._team_news.keys():
             print '%s%s %s %s' % (fg(tcd[team]['fg']), bg(tcd[team]['bg']), team, attr(0))
             for player in self._team_news[team].keys():
